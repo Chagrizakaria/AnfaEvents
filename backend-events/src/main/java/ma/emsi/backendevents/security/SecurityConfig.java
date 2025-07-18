3228967b9e472de03d52d8a19c96f0898bad8537
Chagrizakaria <zikochagr@gmail.com>
2025-07-18 22:26:12 +0100

    Fix: Update CORS policy for production deployment

--- backend-events/src/main/java/ma/emsi/backendevents/security/SecurityConfig.java
+++ backend-events/src/main/java/ma/emsi/backendevents/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://playful-dieffenbachia-52377e.netlify.app"));
+        configuration.addAllowedOrigin("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
